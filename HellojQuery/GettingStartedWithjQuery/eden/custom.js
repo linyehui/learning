7e086e47ec009d5f0cb8cb15d94cafb4b2792f45
linyehui <debehe@gmail.com>
2012-09-14 22:37:56 +0800

    form reset

--- HellojQuery/GettingStartedWithjQuery/eden/custom.js
+++ HellojQuery/GettingStartedWithjQuery/eden/custom.js
@@ -1,9 +1,5 @@
 ﻿$(document).ready(function() {
-	//$("#orderedlist").addClass("red");
-	//$("#orderedlist > li").addClass("blue");
-	$("#orderedlist li:last").hover(function() {
-		$(this).addClass("green");
-	}, function() {
-		$(this).removeClass("green");
+	$("#reset").click(function() {
+		$("form")[0].reset();
 	});
 });
